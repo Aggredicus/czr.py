246798ab12fc881bb8dab85aec544bb6f038b9dd
Aggredicus <dillonmichaellee@gmail.com>
2018-03-27 21:11:27 -0400

    Fix whitespace
    Various indentations did not transfer properly between text editors

--- czr.py
+++ czr.py
@@ -1,56 +1,41 @@
-# czar.py is a Caeser Cipher Tool started by Aggredicus on 3/27/18
+# czr.py is a Caeser Cipher Tool started by Aggredicus on 3/27/18
 
 # The tkinter GUI template was pulled from University of Cape Town with the following attribution:
 # © Copyright 2013, 2014, University of Cape Town and individual contributors. This work is released under the CC BY-SA 4.0 licence. Revision 8e685e710775. 
 
 from tkinter import Tk, Label, Button, Entry, IntVar, END, W, E
 
-class Czr:
+# Caeser Cipher
 
-    # Converts a string containing a letter into an integer representation
-    def num_encrypt(arg):
-        if (arg.isalpha() == True):
-            lowercase_letter = arg.lower()
-            letter_code = ord(lowercase_letter) - 96
-            return int(letter_code)
-        else:
-            return str(arg)
-    # Converts an integer representation of a letter into a string containing a letter
-    def num_decrypt(arg):
-        # Checks to see if arg is a non-integer
-        if (isinstance(arg, int) != True):
-          print('num_decrypt received a non-integer of value ' + arg)
+# Converts a string containing a letter into an integer representation
+def num_encrypt(arg):
+    if (arg.isalpha() == True):
+        lowercase_letter = arg.lower()
+        letter_code = ord(lowercase_letter) - 96
+        return int(letter_code)
+    else:
+        return str(arg)
+    
+# Converts an integer representation of a letter into a string containing a letter
+def num_decrypt(arg):
+    # Checks to see if arg is a non-integer
+    if (isinstance(arg, int) != True):
+        print('num_decrypt received a non-integer of value ' + arg)
         return arg
-        else:
-          # The letter_code variable used to be for converting to int, but I kept it for
-          # aesthetic reasons after implementing the section above.
-          lowercase_letter = chr(arg + 96)
-          return lowercase_letter
+    else:
+        # The letter_code variable used to be for converting to int, but I kept it for
+        # aesthetic reasons after implementing the section above.
+        lowercase_letter = chr(arg + 96)
+        return lowercase_letter
 
-    # Shifts a letter <shift_value> places to the right for each letter
-    def czr_encrypt(shift_value, letter):
-        if (letter.isalpha() == False):
-          return letter
-        else:
-          letter_to_num = num_encrypt(str(letter))
-          shifted_num = letter_to_num + shift_value
-          # Handles cipher shifts past z
-          if (shifted_num > 26):
-              shifted_num -= 26
-          # Handles cipher shifts before a
-          elif (shifted_num < 1):
-              shifted_num += 26
-          num_to_letter = num_decrypt(shifted_num)
-          return num_to_letter
-
-    # Shifts a letter <shift_value> places to the left for each letter
-    def czr_decrypt(shift_value, letter):
-        if (letter.isalpha() == False):
-            return letter
-        else:
-            letter_to_num = num_encrypt(str(letter))
-            shifted_num = letter_to_num - shift_value
-            # Handles cipher shifts past z
+# Shifts a letter <shift_value> places to the right for each letter
+def czr_encrypt(shift_value, letter):
+    if (letter.isalpha() == False):
+        return letter
+    else:
+        letter_to_num = num_encrypt(str(letter))
+        shifted_num = letter_to_num + shift_value
+        # Handles cipher shifts past z
         if (shifted_num > 26):
             shifted_num -= 26
         # Handles cipher shifts before a
@@ -59,18 +44,36 @@ class Czr:
         num_to_letter = num_decrypt(shifted_num)
         return num_to_letter
 
-    # Takes in the Caeser shift value and a full string to shift the full string <shift_value> places to the right
-    def czr_full_encrypt(shift_value, full_string):
-        arr = list(full_string)
-        encrypted_arr = []
-        i = 0
-        while i < len(arr):
-            encrypted_arr.append(czr_encrypt(shift_value, arr[i]))
-            i += 1
-        final_string = ''.join(encrypted_arr)
-        return final_string
+# Shifts a letter <shift_value> places to the left for each letter
+def czr_decrypt(shift_value, letter):
+    if (letter.isalpha() == False):
+        return letter
+    else:
+        letter_to_num = num_encrypt(str(letter))
+        shifted_num = letter_to_num - shift_value
+        # Handles cipher shifts past z
+    if (shifted_num > 26):
+        shifted_num -= 26
+    # Handles cipher shifts before a
+    elif (shifted_num < 1):
+        shifted_num += 26
+    num_to_letter = num_decrypt(shifted_num)
+    return num_to_letter
 
-# Tkinter GUI Code
+# Takes in the Caeser shift value and a full string to shift the full string <shift_value> places to the right
+def czr_full_encrypt(shift_value, full_string):
+    arr = list(full_string)
+    encrypted_arr = []
+    i = 0
+    while i < len(arr):
+        encrypted_arr.append(czr_encrypt(shift_value, arr[i]))
+        i += 1
+    final_string = ''.join(encrypted_arr)
+    return final_string
+
+# Tkinter GUI
+
+class Czr:
 
     def __init__(self, master):
         self.master = master
